6675a4ceadb7643c46f785569ef60a5a2c8672dd
rdk <renaud.dk@gmail.com>
2017-08-03 12:48:55 +0200

    + Add invoice model

---
+++ app/models/invoice.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+__author__ = 'rdk'
+
+from app import db
+
+
+class Invoice(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(64), index = True, unique = True)
+    date = db.Column(db.DateTime)
+    work_days = db.Column(db.Float)
+    price_htva = db.Column(db.Float)
+    price_total = db.Column(db.Float)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable = False)
+    customer = db.relationship('Customer', backref=db.backref('invoices', cascade="all, delete-orphan"), lazy='joined')
+
+    def __repr__(self):
+        return '<Number %r>' % (self.number)
