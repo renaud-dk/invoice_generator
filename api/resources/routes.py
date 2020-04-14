ccd5de16d0f972127ed0011e0dab569d4657f70e
rdk <rdk@genitek.be>
2020-04-14 22:44:07 +0200

    Add project api & URI

--- api/resources/routes.py
+++ api/resources/routes.py
@@ -2,9 +2,13 @@
 
 from .auth import SignupApi, LoginApi
 from .customer import CustomersApi, CustomerApi
+from .project import ProjectsApi, ProjectsCustomerApi, ProjectCustomerApi
 
 def initialize_routes(api):
     api.add_resource(SignupApi, '/api/v1/auth/signup')
     api.add_resource(LoginApi, '/api/v1/auth/login')
     api.add_resource(CustomersApi, '/api/v1/customers')
     api.add_resource(CustomerApi, '/api/v1/customers/<id>')
+    api.add_resource(ProjectsApi, '/api/v1/projects')
+    api.add_resource(ProjectsCustomerApi, '/api/v1/customers/<cust_id>/projects')
+    api.add_resource(ProjectCustomerApi, '/api/v1/customers/<cust_id>/projects/<prj_id>')
