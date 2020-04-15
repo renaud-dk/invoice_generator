b5bb58002b5de444e12c4d97afa176dc667155a3
rdk <rdk@genitek.be>
2020-04-15 20:52:49 +0200

    Add presta api

--- api/resources/routes.py
+++ api/resources/routes.py
@@ -3,6 +3,7 @@
 from .auth import SignupApi, LoginApi
 from .customer import CustomersApi, CustomerApi
 from .project import ProjectsApi, ProjectsCustomerApi, ProjectCustomerApi
+from .presta import PrestasApi, PrestasProjectApi, PrestaProjectApi
 
 def initialize_routes(api):
     api.add_resource(SignupApi, '/api/v1/auth/signup')
@@ -12,3 +13,6 @@ def initialize_routes(api):
     api.add_resource(ProjectsApi, '/api/v1/projects')
     api.add_resource(ProjectsCustomerApi, '/api/v1/customers/<cust_id>/projects')
     api.add_resource(ProjectCustomerApi, '/api/v1/customers/<cust_id>/projects/<prj_id>')
+    api.add_resource(PrestasApi, '/api/v1/prestas')
+    api.add_resource(PrestasProjectApi, '/api/v1/customers/<cust_id>/projects/<prj_id>/prestas')
+    api.add_resource(PrestaProjectApi, '/api/v1/customers/<cust_id>/projects/<prj_id>/prestas/<prst_id>')
