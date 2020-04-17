43a34d88174fdd60cfed75fc149ef6be36a84dee
rdk <rdk@genitek.be>
2020-04-17 22:12:55 +0200

    Add hybrid_property

--- api/database/presta.py
+++ api/database/presta.py
@@ -1,7 +1,9 @@
 #-*- coding: utf-8 -*-
-
+import datetime
 from api.app import db
 from .base_model import BaseModel
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.hybrid import hybrid_property
 
 class Presta(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,3 +15,16 @@ class Presta(BaseModel):
     invoice_number = db.Column(db.String(64), nullable=True)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     project = db.relationship('Project', backref=db.backref('prestas', cascade="all, delete-orphan"), lazy='joined')
+
+    # Fullcalendar
+    @hybrid_property
+    def end(self):
+       return self.date + datetime.timedelta(seconds=self.duration)
+
+    @hybrid_property
+    def start(self):
+       return self.date
+
+    @hybrid_property
+    def title(self):
+       return self.project.code
