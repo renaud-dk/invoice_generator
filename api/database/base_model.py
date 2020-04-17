084038d91b0add70f1ae56203f4f08ef642c1ecb
rdk <rdk@genitek.be>
2020-04-17 22:12:39 +0200

    Include hybrid_property

--- api/database/base_model.py
+++ api/database/base_model.py
@@ -3,6 +3,10 @@
 # inspired from https://wakatime.com/blog/32-flask-part-1-sqlalchemy-models-to-json
 
 from api.app import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.inspection import inspect as sa_inspect
+from sqlalchemy.ext.hybrid import hybrid_property
 
 class BaseModel(db.Model):
     __abstract__ = True
@@ -39,5 +43,10 @@ class BaseModel(db.Model):
             # if check in show or key in default:
             else:
                 ret_data[key] = getattr(self, key)
+
+        # include hybrid_property
+        for item in sa_inspect(self.__class__).all_orm_descriptors:
+            if type(item) == hybrid_property:
+                ret_data[item.__name__] = getattr(self, item.__name__)
 
         return ret_data
